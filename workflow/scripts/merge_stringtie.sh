8081e1b22e7a1058a74a643ae6e63e38351422b4
Phoebe Fei <tof4003@med.cornell.edu>
2023-07-14 14:57:59 -0400

    stringtie merge

--- workflow/scripts/merge_stringtie.sh
+++ workflow/scripts/merge_stringtie.sh
@@ -7,7 +7,9 @@ read -p "Enter condition" cond
 file_path="/athena/nixonlab/scratch/projects/JuneHIVMucosa/results/stringtie"
 cd $file_path
 cond_samples=$(cat $metadata | awk -F "," '{ if ($colnum =="{$cond}") print $1}')
+echo $cond_samples
 sam_list = ( "${cond_samples[@]/%//transcripts.gtf}" )
+echo $sam_list
 printf '%s\n' "${hivpos_list[@]}" > {$colnum}_{$cond}.txt
 cat {$colnum}_{$cond}.txt
 mkdir -p {$colnum}_{$cond}
