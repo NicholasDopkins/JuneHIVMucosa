f7e6b608bc773a803e07cb1126d105fef9fbd8eb
Phoebe Fei <tof4003@med.cornell.edu>
2023-07-20 11:47:55 -0400

    reported error with numpy.int being deprecated. add numpy version requires to telescope yaml

--- workflow/scripts/merge_stringtie.sh
+++ workflow/scripts/merge_stringtie.sh
@@ -1,18 +1,15 @@
-#! /bin/bash -l
-
-#run conda activate stringtie
-read -p "Enter CSV metadata path" metadata
-read -p "Enter column number" colnum
-declare -i colnum
-read -p "Enter condition" cond
-echo "$colnum+1"
-file_path="/athena/nixonlab/scratch/projects/JuneHIVMucosa/results/stringtie"
-cd $file_path
-cond_samples=$(cat $metadata | awk -F "," '{ if ($colnum =="$cond") print $1}')
+# Recording the prompts used to sort and create merged stringtie files based on group
+#(in the envs folder)
+conda env create -n stringtie -f stringtie.yaml
+conda activate stringtie
+cd ../../results/stringtie
+#the awk should be modified according to the colnumber and the condition wanted
+cond_samples=$(cat "../../resources/RunTable_HIV_mucosal.csv" | awk -F "," '{ if ($39 =="HIV-1") print $1}')
 echo $cond_samples[1]
 declare sam_list=( "${cond_samples[@]/%//transcripts.gtf}" )
 echo $sam_list
 printf '%s\n' "${hivpos_list[@]}" > {$colnum}_{$cond}.txt
 cat {$colnum}_{$cond}.txt
 mkdir -p {$colnum}_{$cond}
-srun bash -c 'stringtie --merge -o {$colnum}_{$cond} -G ../../resources/gencode.v38.annotation.gtf {$colnum}_{$cond}.txt'
+#better use a screen -S to have it running in the background
+stringtie --merge -o {$colnum}_{$cond} -G ../../resources/gencode.v38.annotation.gtf {$colnum}_{$cond}.txt
